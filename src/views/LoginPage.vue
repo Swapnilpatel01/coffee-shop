3a2e93b086c3c85be562c6f63254cc9ee86fdb7f
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2023-08-15 21:25:36 -0500

    fix: added box shadow to login card

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -60,6 +60,7 @@
   background-color: white;
   font-family: 'Figtree', sans-serif;
   border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
 }
 
 
