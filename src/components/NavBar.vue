a80ef1487a61f0ec1d6ab6a5ac46063b10a3d6c3
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2023-08-15 20:27:02 -0500

    fix: added sign out notification

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,14 +7,19 @@
         <Dropdown title="Menu" :items="services" class="dropdown"/>
 
         <a href="/"><img class="logo" src="../assets/CoffeeShop.svg" alt="" height="32"></a>
-    
+
         <div class="navbar-icons">
+            
+            <!-- TODO: NEED TO STYLE THIS PROPERLY -->
+            <div class="notification" v-if="notification">
+                {{ notification }}
+            </div> 
 
             <span class="material-symbols-outlined"><a class="navbar-links" href="#menu">Search</a></span>
 
             <!-- <span class="material-symbols-outlined"><a class="navbar-links" href="login" @click="goToLogin">account_circle</a></span> -->
             <!-- <a class="navbar-links" href="login" @click="handleSignOut" v-if="isLoggedIn" style="font-size: large;">Sign Out</a> -->
-            
+                       
 
             <!-- Display sign out icon if user is logged in -->
             <span class="material-symbols-outlined" v-if="isLoggedIn">
@@ -25,12 +30,10 @@
             <span class="material-symbols-outlined" v-else>
                 <a class="navbar-links" href="login" @click="goToLogin">account_circle</a>
             </span>
-            
 
             <div>
                 <span class="material-symbols-outlined"><a class="navbar-links" href="cart" @click="goToCart">shopping_cart</a></span>
             </div>
-
 
         </div>
 
@@ -52,7 +55,8 @@
     },
     setup() {
         const isLoggedIn = ref(false); //changes as auth state changes
-
+        const notification = ref("") 
+        
         let auth;
             onMounted(() => {
             auth = getAuth();
@@ -68,13 +72,25 @@
 
         const handleSignOut = () => {
             signOut(auth).then(() => {
+                isLoggedIn.value = false;
+                notification.value = "You have successfully signed out.";
+                console.log("signed out");
+                
+                setTimeout(() => {
+                    notification.value = '';
+                }, 5000)
+
                 router.push("/");
+            })
+            .catch((error) => {
+                console.error("Error signing out:", error)
             });
         };
 
         return {
             isLoggedIn,
-            handleSignOut
+            handleSignOut,
+            notification
         };
     },
     data() {
@@ -202,6 +218,15 @@ nav .menu-item a{
 .navbar-links {
     text-decoration: none !important;
     color: inherit;
+    cursor: pointer;
+}
+
+.notification {
+  background-color: #e8f5e9; /* A light green shade for successful messages */
+  border: 1px solid #4CAF50; /* Green border */
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 4px;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -11,17 +11,35 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDFGo34x5voN9f1AGnQIClc1gVO-uNc7Y8",
-  authDomain: "coffeeshop-d68de.firebaseapp.com",
-  projectId: "coffeeshop-d68de",
-  storageBucket: "coffeeshop-d68de.appspot.com",
-  messagingSenderId: "710541587395",
-  appId: "1:710541587395:web:710d883660f370329d0376",
-  measurementId: "G-XJ8GQZ02MP"
-};
-initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDFGo34x5voN9f1AGnQIClc1gVO-uNc7Y8",
+//   authDomain: "coffeeshop-d68de.firebaseapp.com",
+//   projectId: "coffeeshop-d68de",
+//   storageBucket: "coffeeshop-d68de.appspot.com",
+//   messagingSenderId: "710541587395",
+//   appId: "1:710541587395:web:710d883660f370329d0376",
+//   measurementId: "G-XJ8GQZ02MP"
+// };
+// initializeApp(firebaseConfig);
 // const analytics = getAnalytics(App);
 
+// -------------------------------------------------------------------
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRB-T_caJeCf7VY7-KqeINB1IsTNnl1pg",
+  authDomain: "coffee-shop-da22d.firebaseapp.com",
+  projectId: "coffee-shop-da22d",
+  storageBucket: "coffee-shop-da22d.appspot.com",
+  messagingSenderId: "985868791781",
+  appId: "1:985868791781:web:6e3441d953d8bfaa20090c",
+  measurementId: "G-YPQ5DN1JRJ"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 createApp(App).use(router).mount('#app');
