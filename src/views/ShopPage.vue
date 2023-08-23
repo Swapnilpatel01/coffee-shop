17217c9279258536e9740cdee7b56626a3ddb073
Preet Patel <54684906+Preetp8@users.noreply.github.com>
2023-08-22 21:27:39 -0500

    added pics

--- src/views/ShopPage.vue
+++ src/views/ShopPage.vue
@@ -17,11 +17,13 @@
     <div class="content">
   
       <div v-for="item in items" :key="item.id" class="shop-item">
-        <!-- <img :src="item.image" alt="item.name"> -->
+        <img :src="require(`@/assets/coffeebags/${item.image}`)" alt="item.name" class="bags">
         <h2>{{ item.name }}</h2>
         <p>{{ item.description }}</p>
-        <p>${{ item.price.toFixed(2) }}</p>
-        <button @click="addToCart(item)">Add to Cart</button>
+        <div class="price-btn-container">
+          <p class="price">${{ item.price.toFixed(2) }}</p>
+          <button @click="addToCart(item)" class="btn">Add to Cart</button>
+        </div>
       </div>
   
     </div>
@@ -57,16 +59,9 @@
 }
 
 
-.content{
-  
-  display: grid;
-  grid-template-columns: repeat(3, 1fr); /* Creates three columns of equal width */
-  gap: 16px; /* Spacing between grid items, adjust as needed */
-  justify-content: center;
-  height: 100vh;
-  padding: 20px;
-  width: 100%;
 
+.bags{
+  width: 50%;
 }
 
 .shop-item {
@@ -87,6 +82,68 @@
   max-width: 100%;
   height: auto;
 }
+
+
+.content{
+  
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* Creates three columns of equal width */
+  gap: 16px; /* Spacing between grid items, adjust as needed */
+  justify-content: center;
+  height: 100vh;
+  padding: 20px;
+  width: 100%;
+
+}
+
+.price-btn-container{
+  background-color: #f9f9f9; 
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 5px;
+  /* gap: 10px; */
+}
+
+.price{
+  font-size: 20px; 
+  font-weight: bold; 
+  color: #BDB2A7; 
+  background-color: #f9f9f9; 
+  padding: 5px 10px; 
+  border-radius: 5px; 
+  /* box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); Slight shadow for depth */
+  margin-top: 10px; /* Space from the description */
+  display: inline-block;
+  margin: 0;
+}
+
+
+
+.btn {
+    background-color: #8c847d; /* Coffee color */
+    color: white;
+    font-weight: bold;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    outline: none;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    /* margin-top: 10px; */
+}
+
+.btn:hover {
+    background-color: #A29B91; /* Darker shade on hover */
+}
+
+.btn:active {
+    transform: scale(0.98); /* Slightly scale down when clicked */
+}
+
+
 
 </style>
 
